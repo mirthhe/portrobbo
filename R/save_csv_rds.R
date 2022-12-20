4a68988d1cbbece16e27a38174fab9401fb255bc
Mirthe <mirtheklaassen@outlook.com>
2022-12-20 17:48:27 +0100

    Added utils library

--- R/save_csv_rds.R
+++ R/save_csv_rds.R
@@ -1,3 +1,16 @@
+#' Title
+#'
+#' @param df dataframe to save as csv and rds
+#' @param output output location including filename
+#' @param rownames rownames or not
+#'
+#' @return A csv and rds file
+#' @export
+#' @importFrom utils write.csv
+#'
+#' @examples
+#' mydata <- data.frame(col1 = c("val1", "val2", "val3"), col2 = c("val1", "val2", "val3"))
+#' save_csv_rds(mydata, "mydata")
 save_csv_rds <- function(df, output, rownames = FALSE){
   write.csv(df, paste0(output, ".csv"), row.names = rownames)
   saveRDS(df, file=paste0(output, ".rds"))
