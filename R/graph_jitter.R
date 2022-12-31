739d4085ed1735f097b7e98e8de684ad1707fec3
Mirthe <mirtheklaassen@outlook.com>
2022-12-31 15:13:22 +0100

    Added function to easily add jitter

---
+++ R/graph_jitter.R
@@ -0,0 +1,12 @@
+#' graph_jitter
+#'
+#' @param numb Amount of jitter
+#'
+#' @return Standard input for geom_point's position
+#' @importFrom ggplot2 position_jitter
+#' @export
+#'
+#' @examples
+graph_jitter <- function(numb){
+  position_jitter(width = numb, height = numb, seed = 123)
+}
