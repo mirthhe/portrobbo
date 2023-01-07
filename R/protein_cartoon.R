5209ad4f4c4860d9eb3827eba911b9402484a080
Mirthe <mirtheklaassen@outlook.com>
2023-01-07 17:11:07 +0100

    Added atom colors

--- R/protein_cartoon.R
+++ R/protein_cartoon.R
@@ -27,10 +27,28 @@ protein_cartoon <- function(pdbobject){
     m_set_style(
       sel = m_sel(ss = "s"), # Style beta sheets
       style = m_style_cartoon(color = "#3aff3a", arrows = TRUE)) %>%
-    # Style the alpha helix
     m_set_style(
       sel = m_sel(ss = "h"), # Style alpha helix
       style = m_style_cartoon(color = "#3aff3a")) %>%
+    # Set style of atoms
+    m_set_style(
+      sel = m_sel(elem = "C"), # Style carbon
+      style = m_style_cartoon(color = "#33ff33")) %>%
+    m_set_style(
+      sel = m_sel(elem = "N"), # Style nitrogen
+      style = m_style_cartoon(color = "#3333ff")) %>%
+    m_set_style(
+      sel = m_sel(elem = "O"), # Style oxygen
+      style = m_style_cartoon(color = "#ff4c4c")) %>%
+    m_set_style(
+      sel = m_sel(elem = "S"), # Style sulfur
+      style = m_style_cartoon(color = "#e5c53f")) %>%
+    m_set_style(
+      sel = m_sel(elem = "P"), # Style phosphorus
+      style = m_style_cartoon(color = "#ff7f00")) %>%
+    m_set_style(
+      sel = m_sel(elem = "MG"), # Style magnesium
+      style = m_style_cartoon(color = "#8aff00")) %>%
     # Rotate the scene by given angle on given axis
     m_rotate(angle = 90, axis = "y")
 }
