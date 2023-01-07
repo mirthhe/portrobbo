a1aa66e656bf869990f8c08c921161bc0086eb86
Mirthe <mirtheklaassen@outlook.com>
2023-01-07 16:46:39 +0100

    Added PyMOL colors

--- R/protein_stick.R
+++ R/protein_stick.R
@@ -1,6 +1,6 @@
 #' protein_stick
 #'
-#' @param mydata R object of PDB file made using readLines
+#' @param pdbobject R object of PDB file made using readLines
 #'
 #' @return 3D structural visualization of the protein in stick style
 #' @export
@@ -8,7 +8,7 @@
 #'
 #' @examples
 #' protein_stick(kinesin)
-protein_stick <- function(mydata){
+protein_stick <- function(pdbobject){
   r3dmol(
     viewer_spec = m_viewer_spec(
       # How far user can zoom in
@@ -20,11 +20,31 @@ protein_stick <- function(mydata){
       disableFog = TRUE,
       backgroundColor = "black")) %>%
     # Adding the protein
-    m_add_model(data = mydata, format = "pdb") %>%
+    m_add_model(data = pdbobject, format = "pdb") %>%
     # Center the protein at first
     m_zoom_to() %>%
-    # Set style of structures
-    m_set_style(style = m_style_stick(color = "#3aff3a")) %>%
+    # Set style of atoms
+    m_set_style(
+      sel = m_sel(elem = "C"), # Style carbon
+      style = m_style_stick(color = "#33ff33")) %>%
+    m_set_style(
+      sel = m_sel(elem = "N"), # Style nitrogen
+      style = m_style_stick(color = "#3333ff")) %>%
+    m_set_style(
+      sel = m_sel(elem = "O"), # Style oxygen
+      style = m_style_stick(color = "#ff4c4c")) %>%
+    m_set_style(
+      sel = m_sel(elem = "S"), # Style sulfur
+      style = m_style_stick(color = "#e5c53f")) %>%
+    m_set_style(
+      sel = m_sel(elem = "P"), # Style phosphorus
+      style = m_style_stick(color = "#ff7f00")) %>%
+    m_set_style(
+      sel = m_sel(elem = "MG"), # Style magnesium
+      style = m_style_stick(color = "#8aff00")) %>%
+    m_set_style(
+      sel = m_sel(elem = "H"), # Style hydrogen
+      style = m_style_stick(color = "#E5E5E5")) %>%
     # Rotate the scene by given angle on given axis
     m_rotate(angle = 90, axis = "y")
 }
